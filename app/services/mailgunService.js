d5fbd8292ac3e6ad0c22f867a83dc05387396d3d
Stoyan Petrov <stoyanpetrov@mail.bg>
2021-01-29 09:49:19 +0200

    minor change

--- app/services/mailgunService.js
+++ app/services/mailgunService.js
@@ -11,7 +11,7 @@ module.exports = (config) => {
       //field 'to' is left hardcoded from config for now because I am using the free acount 
       //which is sandboxed and can send emails to up to 3 verified!! email addresses
       const data = {
-        from: "Mailgun Sandbox <postmaster@sandboxe38400d7bff1401f9bc3ca117dbef6dd.mailgun.org>",
+        from: config.mailgun.from ,
         to: config.mailgun.to,
         subject: emailDefinition.subject,
         text: emailDefinition.body
