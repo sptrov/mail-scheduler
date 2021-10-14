5322ff5ac791ec3851afff52c3d2845b7c2ba939
Stoyan Petrov <stoyanpetrov@mail.bg>
2021-10-14 15:01:51 +0300

    formatting

--- app/services/schedulerService.js
+++ app/services/schedulerService.js
@@ -1,8 +1,7 @@
-
 const dbOperations = require('./databaseServices/dbOperations');
 const optionGenerator = require('./queueOptionGenerator')
 
-module.exports =  (sendMailQueue,congig) => {
+module.exports = (sendMailQueue, congig) => {
   sendInstantEmails()
   scheduleCurrentYearEmails()
   scheduleHourlyEmails()
@@ -13,118 +12,130 @@ module.exports =  (sendMailQueue,congig) => {
 
   function sendInstantEmails() {
     dbOperations.getInstantEmails().then((res) => {
-      res.forEach(async email => {
-        sendMailQueue.add({id: email.id});
-        await  dbOperations.updateScheduledEmail(email)
-        console.log('instant------>: ' + email.id)
-      });
-    })
-    .then(()=>{
-      console.log('instant------------>')
-      setTimeout(() => {
-        sendInstantEmails()
-      }, congig.schedulerTimeout);
-    })
+        res.forEach(async email => {
+          sendMailQueue.add({
+            id: email.id
+          });
+          await dbOperations.updateScheduledEmail(email)
+          console.log('instant------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('instant------------>')
+        setTimeout(() => {
+          sendInstantEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   function scheduleCurrentYearEmails() {
     dbOperations.getLaterEmailsForCurrentYear().then((res) => {
-      res.forEach(async (email) => {
+        res.forEach(async (email) => {
           const scheduleOptions = optionGenerator.forSingleLater(email)
           // sendMailQueue.add(email, cronOption);
-          sendMailQueue.add({id: email.id}, scheduleOptions);
+          sendMailQueue.add({
+            id: email.id
+          }, scheduleOptions);
 
-          await  dbOperations.updateScheduledEmail(email)
-        
-            console.log('scheduled_later------>: ' + email.id)        
-      });
-    })
-    .then(()=>{
-      console.log('single_later------------>')
-      setTimeout(() => {
-        scheduleCurrentYearEmails()
-      }, congig.schedulerTimeout);
-    })
+          await dbOperations.updateScheduledEmail(email)
+
+          console.log('scheduled_later------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('single_later------------>')
+        setTimeout(() => {
+          scheduleCurrentYearEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   function scheduleHourlyEmails() {
     dbOperations.getHourlyEmails().then((res) => {
-      res.forEach(async (email) => {
+        res.forEach(async (email) => {
           const scheduleOptions = optionGenerator.forRecurringHourly(email)
           // sendMailQueue.add(email, cronOption);
-          sendMailQueue.add({id: email.id}, scheduleOptions);
+          sendMailQueue.add({
+            id: email.id
+          }, scheduleOptions);
 
-          await  dbOperations.updateScheduledEmail(email)
-        
-            console.log('scheduled_hourly------>: ' + email.id)        
-      });
-    })
-    .then(()=>{
-      console.log('hourly------------>')
-      setTimeout(() => {
-        scheduleHourlyEmails()
-      }, congig.schedulerTimeout);
-    })
+          await dbOperations.updateScheduledEmail(email)
+
+          console.log('scheduled_hourly------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('hourly------------>')
+        setTimeout(() => {
+          scheduleHourlyEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   function scheduleDailyEmails() {
     dbOperations.getDailyEmails().then((res) => {
-      res.forEach(async (email) => {
+        res.forEach(async (email) => {
           const scheduleOptions = optionGenerator.forRecurringDaily(email)
           // sendMailQueue.add(email, cronOption);
-          sendMailQueue.add({id: email.id}, scheduleOptions);
+          sendMailQueue.add({
+            id: email.id
+          }, scheduleOptions);
 
-          await  dbOperations.updateScheduledEmail(email)
-        
-            console.log('scheduled_daily------>: ' + email.id)        
-      });
-    })
-    .then(()=>{
-      console.log('daily------------>')
-      setTimeout(() => {
-        scheduleDailyEmails()
-      }, congig.schedulerTimeout);
-    })
+          await dbOperations.updateScheduledEmail(email)
+
+          console.log('scheduled_daily------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('daily------------>')
+        setTimeout(() => {
+          scheduleDailyEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   function scheduleWeeklyEmails() {
     dbOperations.getWeeklyEmails().then((res) => {
-      res.forEach(async (email) => {
+        res.forEach(async (email) => {
           const scheduleOptions = optionGenerator.forRecurringWeekly(email)
           // sendMailQueue.add(email, cronOption);
-          sendMailQueue.add({id: email.id}, scheduleOptions);
+          sendMailQueue.add({
+            id: email.id
+          }, scheduleOptions);
 
-          await  dbOperations.updateScheduledEmail(email)
-        
-            console.log('scheduled_weekly------>: ' + email.id)        
-      });
-    })
-    .then(()=>{
-      console.log('weekly------------>')
-      setTimeout(() => {
-        scheduleWeeklyEmails()
-      }, congig.schedulerTimeout);
-    })
+          await dbOperations.updateScheduledEmail(email)
+
+          console.log('scheduled_weekly------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('weekly------------>')
+        setTimeout(() => {
+          scheduleWeeklyEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   function scheduleMonthlyEmails() {
     dbOperations.getMonthlyEmails().then((res) => {
-      res.forEach(async (email) => {
+        res.forEach(async (email) => {
           const scheduleOptions = optionGenerator.forRecurringMonthly(email)
           // sendMailQueue.add(email, cronOption);
-          sendMailQueue.add({id: email.id}, scheduleOptions);
+          sendMailQueue.add({
+            id: email.id
+          }, scheduleOptions);
 
-          await  dbOperations.updateScheduledEmail(email)
-        
-            console.log('scheduled_monthly------>: ' + email.id)        
-      });
-    })
-    .then(()=>{
-      console.log('monthly------------>')
-      setTimeout(() => {
-        scheduleMonthlyEmails()
-      }, congig.schedulerTimeout);
-    })
+          await dbOperations.updateScheduledEmail(email)
+
+          console.log('scheduled_monthly------>: ' + email.id)
+        });
+      })
+      .then(() => {
+        console.log('monthly------------>')
+        setTimeout(() => {
+          scheduleMonthlyEmails()
+        }, congig.schedulerTimeout);
+      })
   }
 
   return {
@@ -135,4 +146,3 @@ module.exports =  (sendMailQueue,congig) => {
     scheduleMonthlyEmails: scheduleMonthlyEmails
   }
 }
-
